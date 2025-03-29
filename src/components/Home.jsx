02bf7fefe0710bd04234e59cf50a5f3a20296d6f
parth <parth@parthjoshi2973@gmail.com>
2025-03-29 11:35:48 +0530

    added home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,11 @@
-import { useState, Suspense } from "react";
+import { useState, useRef, useEffect, Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import {
   OrbitControls,
   Loader,
   GradientTexture,
   Float,
+  Wireframe,
 } from "@react-three/drei";
 import { Button3D } from "./ui-3d/button-3d";
 import { Card3D } from "./ui-3d/card-3d";
@@ -13,23 +14,55 @@ import { Menu3D } from "./ui-3d/menu-3d";
 import { Text } from "@react-three/drei";
 import { ParticleSystem } from "./ui-3d/particle-system";
 import { useNavigate } from "react-router-dom";
+import * as THREE from "three";
+
+function FloatingShape({ shape, position, color }) {
+  return (
+    <Float speed={2} rotationIntensity={0.5} floatIntensity={1.5}>
+      <mesh position={position}>
+        {/* Define Shape */}
+        {shape === "box" && <boxGeometry args={[1, 2.5, 1.3,10]} />}
+        {shape === "sphere" && <sphereGeometry args={[1, 32, 35]} />}
+        {shape === "cone" && <coneGeometry args={[1, 1.7, 32]} />}
+        {shape === "torus" && <torusGeometry args={[1.4, 0.2, 16, 100]} />}
+        {shape === "cylinder" && <cylinderGeometry args={[1, 1, 1.6, 32]} />}
+        {shape === "dodecahedron" && (
+          <dodecahedronGeometry args={[1.5, 0, 1]} />
+        )}
+        {shape === "torus-knot" && <torusKnotGeometry />}
+        {shape === "octahedron" && <octahedronGeometry args={[1,1,1,-1]} />}
+        {/* Apply GradientTexture */}
+        <meshStandardMaterial  wireframe>
+          <GradientTexture
+            attach="map"
+            stops={[0, 1]}
+            colors={["#FF5733", color]} 
+            size={1024}
+          />
+        </meshStandardMaterial>
+      </mesh>
+    </Float>
+  );
+}
 
 export default function Home() {
+  const navigate = useNavigate();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <div className="absolute top-10 right-4 z-10 flex gap-2"></div>
 
       <div className="w-full flex justify-center items-center h-screen">
         <Canvas
-          className="bg-gradient-to-b from-[#0D1B2A] via-[#0D1B2A] to-[#000]"
+          className="bg-black"
           camera={{
             position: [1, 2, 8],
-            fov: window.innerWidth < 768 ? 60 : 35,
+            fov: window.innerWidth < 768 ? 75 : 35,
           }}
         >
           <mesh>
             <GradientTexture attach="map" stops={[0, 1]} colors={["#00FF00"]} />
             <Suspense>
+              <meshBasicMaterial wireframe="true" />
               <ComponentsShowcase />
             </Suspense>
           </mesh>
@@ -45,6 +78,12 @@ export default function Home() {
           />
         </Canvas>
       </div>
+      <div
+        className=" absolute text-3xl cursor-pointer"
+        onClick={() => navigate("/chatbot")}
+      >
+        Chatbot
+      </div>
       <Loader />
     </main>
   );
@@ -54,56 +93,43 @@ function ComponentsShowcase() {
   const navigate = useNavigate();
   return (
     <group>
-      <Float position={[0, 0, 0]} speed={6} floatIntensity={0.1}>
-        <Button3D
-          position={[-2, 0.5, 0]}
-          onClick={() => navigate("/uploadModel")}
-          label="Upload Your Model"
-          color={"#ed3b65"}
-          hoverColor={"#7C3AED"}
-        />
-        <Menu3D
-          position={[0, 2, 0]}
-          items={[
-            { label: "Home", value: "home" },
-            { label: "3D shapes", value: "3D shapes" },
-            { label: "3D Models", value: "3D Models" },
-          ]}
-        />
-        <Card3D
-          position={[0, 0.5, 0]}
-          rotation={[0, 0, 0]}
-          width={1.5}
-          height={1}
-          depth={0.1}
-          color={"#F1F5F9"}
-          title="Cross 3D"
-          content="Your Gateway to Seamless 3D Integration."
-        />
+      {/* <Float position={[0, 0, 0]} speed={6} floatIntensity={0.1}> */}
+      <Button3D
+        position={[-1, -0.4  , 0]}
+        onClick={() => navigate("/uploadModel")}
+        label="Login"
+        color={"#ed3b65"}
+        hoverColor={"#7C3AED"}
+      />
+      <Button3D
+        position={[0.8, -0.4  , 0]}
+        onClick={() => navigate("/uploadModel")}
+        label="Sign Up"
+        color={"#ed3b65"}
+        hoverColor={"#7C3AED"}
+      />
+      {/* <Menu3D
+        position={[0, 2, 0]}
+        items={[
+          { label: "Home", value: "home" },
+          { label: "3D shapes", value: "3D shapes" },
+          { label: "3D Models", value: "3D Models" },
+        ]}  
+      /> */}
 
-        <Loader3D
-          position={[2, 0.5, 0]}
-          size={0.5}
-          speed={1.5}
-          color={"#DB2777"}
-        />
-        <ParticleSystem
-          count={1000}
-          color={"#fff"}
-          size={0.15}
-          speed={0.0005}
-        />
+      <ambientLight intensity={2} />
+      <ParticleSystem count={1000} color={"#fff"} size={0.15} speed={0.0005} />
       <Text
-        position={[0, -1, 0]}
-        fontSize={0.15}
-        color="#00FF00"
+        position={[0, 0, 0]}
+        fontSize={0.75}
+        color="#FFFFFF"
         anchorX="center"
         anchorY="middle"
         depthWrite={false}
       >
-        Before using any shape or model make sure you download three js first   
+        Cross 3D
       </Text>
-      <Text
+      {/* <Text
         position={[0, -1.3, 0]}
         fontSize={0.15}
         color="#00FF00"
@@ -111,9 +137,21 @@ function ComponentsShowcase() {
         anchorY="middle"
         depthWrite={false}
       >
-        Refer to the documentation for more information 
-      </Text>
-      </Float>
+        Refer to the documentation for more information
+      </Text> */}
+      <FloatingShape shape="box" position={[-4, 2.5, 0]} color="red" />
+      <FloatingShape shape="sphere" position={[4, 2.7, 0]} color="blue" />
+      <FloatingShape shape="cone" position={[-4, -2, 0]} color="green" />
+      <FloatingShape shape="torus" position={[3, -3, 0]} color="purple" />
+      <FloatingShape
+        shape="cylinder"
+        position={[-5.5, -0.5, -2]}
+        color="orange"
+      />
+      <FloatingShape shape="dodecahedron" position={[4, 0, -2]} color="cyan" />
+      <FloatingShape shape="torus-knot" position={[0, 2.7, -1 ]} color="yellow" />
+      <FloatingShape shape="octahedron" position={[0, -2.5, -1 ]} color="yellow" />
+      {/* </Float> */}
     </group>
   );
 }
